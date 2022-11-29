2b55063f18efe83a2163f715fad0c0789439f9b3
chenxuanfeng <chenxuanfeng@meituan.com>
2022-11-29 16:39:44 +0800

    feat: 添加 eslint

--- middleware.ts
+++ middleware.ts
@@ -3,17 +3,19 @@ import type { NextFetchEvent, NextRequest } from "next/server";
 import log from "./util/print-log";
 
 export function middleware(req: NextRequest, ev: NextFetchEvent) {
-  const response = NextResponse.next();
+    const response = NextResponse.next();
 
-  const allCookies = req.cookies.getAll()
-  const isNextjs = req.cookies.has('nextjs')
-  response.headers.set("x-modified-edge", "true");
-  
-  log.info(`执行请求中间件: ${allCookies.length} ${isNextjs}`)
+    const allCookies = req.cookies.getAll();
+    const isNextjs = req.cookies.has("nextjs");
+    response.headers.set("x-modified-edge", "true");
 
-  return response;
+    log.info(
+        `执行请求中间件: ${ev.sourcePage}, ${allCookies.length} ${isNextjs}`
+    );
+
+    return response;
 }
 
 export const config = {
-  matcher: ['/'],
-}
+    matcher: ["/"],
+};
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,45 +1,41 @@
 import Document, {
-  Html,
-  Head,
-  Main,
-  NextScript,
-  DocumentContext,
-  DocumentInitialProps,
+    Html,
+    Head,
+    Main,
+    NextScript,
+    DocumentContext,
+    DocumentInitialProps,
 } from "next/document";
 
 interface IProps extends DocumentInitialProps {
-  bodyClassName: string;
+    bodyClassName: string;
 }
 
 export default class AppDocument extends Document<IProps> {
+    static async getInitialProps(ctx: DocumentContext): Promise<IProps> {
+        const props = await Document.getInitialProps(ctx);
 
-  static async getInitialProps(ctx: DocumentContext): Promise<IProps> {
+        console.log(`执行 Document getInitialProps ${Date.now()}`);
 
-    const props = await Document.getInitialProps(ctx);
-    console.log(`执行 Document getInitialProps ${Date.now()}`);
+        return { ...props, bodyClassName: "body-test" };
+    }
 
-    return { ...props, bodyClassName: "body-test" };
-  }
-
-  render(): JSX.Element {
-    return (
-      <Html>
-        <Head>
-          <style>
-            {
-              `.body {
+    render(): JSX.Element {
+        return (
+            <Html>
+                <Head>
+                    <style>
+                        {`.body {
                   background-color: red
-                }`
-            }
-          </style>
-        </Head>
+                }`}
+                    </style>
+                </Head>
 
-        <body className={this.props.bodyClassName}>
-          <Main />
-          <NextScript></NextScript>
-        </body>
-        
-      </Html>
-    );
-  }
+                <body className={this.props.bodyClassName}>
+                    <Main />
+                    <NextScript />
+                </body>
+            </Html>
+        );
+    }
 }
