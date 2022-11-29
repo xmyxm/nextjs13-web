1d8cd601ac8b1c8017e212f053fb9d077de5bb4c
chenxuanfeng <chenxuanfeng@meituan.com>
2022-11-29 17:11:21 +0800

    up

---
+++ config/router.ts
@@ -0,0 +1,37 @@
+export const routerList = [
+    {
+        title: "动态路由",
+        content: "动态页面示例",
+        src: "/num/1",
+    },
+    {
+        title: "CSR",
+        content: "客户端渲染页面示例",
+        src: "/csr",
+    },
+    {
+        title: "SSR",
+        content: "服务端渲染示例",
+        src: "/page/ssr",
+    },
+    {
+        title: "SSG",
+        content: "静态生成示例",
+        src: "/ssg",
+    },
+    {
+        title: "动态SSG",
+        content: "动态生成静态页面示例",
+        src: "/doc/1",
+    },
+    {
+        title: "ISR",
+        content: "增量静态生成",
+        src: "/isr",
+    },
+    {
+        title: "魔法渲染",
+        content: "getStaticProps",
+        src: "/magic",
+    },
+];
